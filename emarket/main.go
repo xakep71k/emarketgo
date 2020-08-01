a8adf569b291f92fb2a6f0f43304bd2a0398b50d
Alexander <alexander.bubnov@gmail.com>
2020-08-01 23:17:53 +0300

    code made more occurate

--- emarket/main.go
+++ emarket/main.go
@@ -21,17 +21,32 @@ const (
 	PageContact
 )
 
-type WEBPage struct {
+const (
+	URLHome     = "/"
+	URLDelivery = "/dostavka"
+	URLContact  = "/kontakty"
+)
+
+const (
+	TitleHome     = "Журналы"
+	TitleDelivery = "Доставка"
+	TitleContact  = "Контакты"
+	TitleNotFound = "Страница не найдена"
+)
+
+type EMarketPage struct {
 	Title       string
 	Body        string
 	CurrentPage int
 }
 
-var WEBRoot string
-var ContentTypeHash map[string]string
+type Content struct {
+	mytype map[string]string
+}
 
-func init() {
-	ContentTypeHash = map[string]string{
+func NewContent() *Content {
+	c := &Content{}
+	c.mytype = map[string]string{
 		".js":     "application/javascript",
 		".css":    "text/css",
 		".ico":    "image/x-icon",
@@ -41,10 +56,34 @@ func init() {
 		".woff2":  "font/woff2",
 		".ttf":    "font/ttf",
 	}
+	return c
 }
 
-func ContentType(filename string) string {
-	for suffix, contentType := range ContentTypeHash {
+type EMarket struct {
+	rootDir string
+	content *Content
+	router  *http.ServeMux
+}
+
+func NewEMarket(rootDir string) *EMarket {
+	e := &EMarket{
+		rootDir: rootDir,
+		content: NewContent(),
+	}
+	router := http.NewServeMux()
+	router.HandleFunc("/favicon.ico", e.handleFavicon)
+	router.HandleFunc("/bootstrap/", e.handleFile)
+	router.HandleFunc("/fontawesome/", e.handleFile)
+	router.HandleFunc("/static/", e.handleFile)
+	router.HandleFunc(URLHome, e.home)
+	router.HandleFunc(URLDelivery, e.Delivery)
+	router.HandleFunc(URLContact, e.ContactUs)
+	e.router = router
+	return e
+}
+
+func (c *Content) DetectType(filename string) string {
+	for suffix, contentType := range c.mytype {
 		if strings.HasSuffix(filename, suffix) {
 			return contentType
 		}
@@ -53,57 +92,56 @@ func ContentType(filename string) string {
 	panic(fmt.Sprintf("unknown type %v", filename))
 }
 
-func NotFound(w http.ResponseWriter, r *http.Request) {
+func (e *EMarket) notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Printf("not found %v\n", r.URL.Path)
-	p := WEBPage{Title: "Страница не найдена", Body: html.NotFound}
+	p := EMarketPage{Title: TitleNotFound, Body: html.NotFound}
 	html.AppPage.Execute(w, p)
 }
 
-func Magazine(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "inside magazine")
-}
-
-func Root(w http.ResponseWriter, r *http.Request) {
+func (e *EMarket) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		NotFound(w, r)
+		e.notFound(w, r)
 	} else {
-		p := WEBPage{Title: "test title", CurrentPage: PageHome}
+		p := EMarketPage{Title: TitleHome, CurrentPage: PageHome}
 		html.AppPage.Execute(w, p)
 	}
 }
 
-func HandleFile(w http.ResponseWriter, r *http.Request) {
-	body, err := readFile(r.URL.Path)
+func (e *EMarket) handleCustomFile(filename string, w http.ResponseWriter, r *http.Request) {
+	body, err := readFile(e.rootDir + r.URL.Path)
 	if err == nil {
-		w.Header().Set("Content-Type", ContentType(r.URL.Path))
+		w.Header().Set("Content-Type", e.content.DetectType(r.URL.Path))
 		w.Write(body)
 	} else {
-		NotFound(w, r)
+		e.notFound(w, r)
 	}
 }
 
-func Favicon(w http.ResponseWriter, r *http.Request) {
-	body, err := readFile("/static/favicon.ico")
-	if err == nil {
-		w.Header().Set("Content-Type", ContentType(r.URL.Path))
-		w.Write(body)
-	}
+func (e *EMarket) handleFile(w http.ResponseWriter, r *http.Request) {
+	e.handleCustomFile(r.URL.Path, w, r)
 }
 
-func Delivery(w http.ResponseWriter, r *http.Request) {
-	p := WEBPage{Title: "Доставка", CurrentPage: PageDelivery}
+func (e *EMarket) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	e.router.ServeHTTP(w, r)
+}
+
+func (e *EMarket) handleFavicon(w http.ResponseWriter, r *http.Request) {
+	e.handleCustomFile("/static/favicon.ico", w, r)
+}
+
+func (e *EMarket) Delivery(w http.ResponseWriter, r *http.Request) {
+	p := EMarketPage{Title: TitleDelivery, CurrentPage: PageDelivery}
 	html.AppPage.Execute(w, p)
 }
 
-func ContactUs(w http.ResponseWriter, r *http.Request) {
-	p := WEBPage{Title: "Контакты", CurrentPage: PageContact}
+func (e *EMarket) ContactUs(w http.ResponseWriter, r *http.Request) {
+	p := EMarketPage{Title: TitleContact, CurrentPage: PageContact}
 	html.AppPage.Execute(w, p)
 }
 
 func readFile(filename string) ([]byte, error) {
-	filename = WEBRoot + filename
 	stat, err := os.Stat(filename)
 	if err != nil {
 		fmt.Printf("read file: %s %v\n", filename, err)
@@ -123,36 +161,29 @@ func readFile(filename string) ([]byte, error) {
 }
 
 func main() {
-	var err error
 	if len(os.Args) != 2 {
 		fmt.Printf("Usage: %s --web-root <path>\n", os.Args[0])
 		os.Exit(1)
 	}
-	webRoot := flag.String("web-root", "", "<path>")
+	webRootOpt := flag.String("web-root", "", "<path>")
 	flag.Parse()
 
-	if webRoot == nil || *webRoot == "" {
+	if webRootOpt == nil || *webRootOpt == "" {
 		fmt.Println("web root not specified")
 		os.Exit(1)
 	}
 
-	WEBRoot, err = filepath.Abs(*webRoot)
+	webRoot, err := filepath.Abs(*webRootOpt)
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 		return
 	}
 
-	router := http.NewServeMux()
-	router.HandleFunc("/favicon.ico", Favicon)
-	router.HandleFunc("/bootstrap/", HandleFile)
-	router.HandleFunc("/fontawesome/", HandleFile)
-	router.HandleFunc("/static/", HandleFile)
-	router.HandleFunc("/", Root)
-	router.HandleFunc("/dostavka", Delivery)
-	router.HandleFunc("/kontakty", ContactUs)
+	emarket := NewEMarket(webRoot)
 	srv := &http.Server{
-		Handler:      router,
+		Handler:      emarket,
 		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
